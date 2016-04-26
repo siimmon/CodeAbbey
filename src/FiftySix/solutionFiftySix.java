b99e2afb06332413911d831290a8cef18623598c
Simon Roth <siimmon@hotmail.com>
2016-04-27 00:29:31 +0100

    finish - great puzzle

--- src/FiftySix/solutionFiftySix.java
+++ src/FiftySix/solutionFiftySix.java
@@ -13,11 +13,11 @@ class solutionFiftySix {
 
         boolean[][] map = new boolean[mapSize][mapSize];
 
-        String line =   "-------\n"+
-                "---XX--\n"+
-                "-XXX---\n"+
-                "-------\n"+
-                "-------";
+        String line =   "--XXX--\n"+
+                        "-X--XX-\n"+
+                        "-X-X-X-\n"+
+                        "--XXXX-\n"+
+                        "--XX--X";
         String[] lineSplit = line.split("\n");
         // put the 7x5 map from input in from 50x50 so the edges dont mess things up
 
@@ -30,32 +30,80 @@ class solutionFiftySix {
             }
         }
 
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if(map[j][i]) {
-                    System.out.print("X");
+        for(int x = 0; x < 5; x++) {
+            game(map);
+            int count = 0;
+
+            for (int i = 0; i < 100; i++) {
+                for (int j = 0; j < 100; j++) {
+                    if (map[j][i]) {
+                        //System.out.print("X");
+                        count++;
+                    }
+                    else {
+                  //      System.out.print("-");
+                    }
                 }
-                else {
-                    System.out.print("-");
-                }
+                //System.out.println("");
             }
-            System.out.println("");
+            System.out.print(count+" ");
         }
+
     }
 
     public static void game(boolean[][] mapIn) {
         // mark deads
-        boolean[][] deadMarks = mapIn;
+        boolean[][] deadMarks = new boolean[mapSize][mapSize];
+        boolean[][] newBorns = new boolean[mapSize][mapSize];
 
-        for (int i = 0; i < mapSize; i++) {
-            for (int j = 0; j < mapSize; j++) {
+        for (int i = 1; i < mapSize-1; i++) {
+            for (int j = 1; j < mapSize-1; j++) {
                 int neighbours = 0;
 
-                if(mapIn[j][i]) {
-                    // we're here if()
+                if(mapIn[j+1][i]) {
+                    neighbours++;
+                }
+                if(mapIn[j+1][i+1]) {
+                    neighbours++;
+                }
+                if(mapIn[j][i+1]) {
+                    neighbours++;
+                }
+                if(mapIn[j-1][i+1]) {
+                    neighbours++;
+                }
+                if(mapIn[j-1][i]) {
+                    neighbours++;
+                }
+                if(mapIn[j-1][i-1]) {
+                    neighbours++;
+                }
+                if(mapIn[j][i-1]) {
+                    neighbours++;
+                }
+                if(mapIn[j+1][i-1]) {
+                    neighbours++;
+                }
+
+                if((neighbours < 2 || neighbours > 3) && mapIn[j][i]) {
+                    deadMarks[j][i] = true;
+                }
+
+                if(neighbours == 3 && !mapIn[j][i]) {
+                    newBorns[j][i] = true;
                 }
             }
         }
 
+        for (int i = 0; i < mapSize; i++) {
+            for (int j = 0; j < mapSize; j++) {
+                if(deadMarks[j][i]) {
+                    mapIn[j][i] = false;
+                }
+                if(newBorns[j][i]) {
+                    mapIn[j][i] = true;
+                }
+            }
+        }
     }
 }
